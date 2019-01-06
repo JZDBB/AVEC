f946c1960683a5830fb9b4deb0ff9212885b80c2
XuYining <oxuyining@gmail.com>
2019-01-06 15:20:17 +0800

    move and assert
    Signed-off-by: XuYining <oxuyining@gmail.com>

--- gensim/move.py
+++ gensim/move.py
@@ -1,6 +1,7 @@
 import os
+import shutil
 
-a = '../data/audio'
+a = '../data/audio/audio'
 b = '../data/10001'
 
 files = os.listdir(a)
@@ -8,7 +9,12 @@ files = os.listdir(a)
 for file in files:
     name = file.split('_')[0]
     full_path = os.path.join(b, name + '_P')
-    cmd = 'mkdir ' + full_path
-    os.system(cmd)
-    cmd = 'cp ' + os.path.join(a, file) + ' ' + full_path
-    os.system(cmd)
+    # Windows下操作
+    os.mkdir(full_path)
+    shutil.copy(os.path.join(a, file), full_path)
+    
+    # ubuntu下操作
+    # cmd = 'mkdir ' + full_path
+    # os.system(cmd)
+    # cmd = 'cp ' + os.path.join(a, file) + ' ' + full_path
+    # os.system(cmd)
